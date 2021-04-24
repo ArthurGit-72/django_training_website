f7548746776cabedc062a09312feffcbb1de9a0e
ArthurGit-72 <https://github.com/ArthurGit-72/django_training_website.git>
2021-04-24 22:27:14 +0300

    first_commit

--- foto/urls.py
+++ foto/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+#from .views import first_page # указываем по какому пути находится приложение
+from crm import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.first_page)
+    #path('', first_page), # пустой путь(быдет написано сражу после имени домена)
+
 ]
